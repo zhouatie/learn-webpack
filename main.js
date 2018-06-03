8fe4f7b681ed725f16b9f6d994a3bc709a88e4e4
zhoust <zhoust@guahao.com>
2018-06-03 23:12:37 +0800

    update

---
+++ main.js
@@ -0,0 +1,11 @@
+// // 通过 CommonJS 规范导入 show 函数
+// const show = require('./show.js');
+// // 执行 show 函数
+// show('Webpack');
+
+// 通过 CommonJS 规范导入 CSS 模块
+require('./main.css');
+// 通过 CommonJS 规范导入 show 函数
+const show = require('./show.js');
+// 执行 show 函数
+show('Webpack');
