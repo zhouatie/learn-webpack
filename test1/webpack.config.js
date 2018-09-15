ca551f639af4c046115d9f542bcbd848781777bf
zhoust <zhoust@guahao.com>
2018-09-15 22:41:15 +0800

    finish study base

--- test1/webpack.config.js
+++ test1/webpack.config.js
@@ -15,17 +15,18 @@ module.exports = {
       {
         // 用正则去匹配要用该 loader 转换的 CSS 文件
         test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          // 转换 .css 文件需要使用的 Loader
-          use: ['css-loader'],
-        }),
+        loaders: ['style-loader', 'css-loader'],
+        // use: ExtractTextPlugin.extract({
+        //   // 转换 .css 文件需要使用的 Loader
+        //   use: ['css-loader'],
+        // }),
       }
     ]
   },
-  plugins: [
-    new ExtractTextPlugin({
-      // 从 .js 文件中提取出来的 .css 文件的名称
-      filename: `[name]_[contenthash:8].css`,
-    }),
-  ]
+  // plugins: [
+  //   new ExtractTextPlugin({
+  //     // 从 .js 文件中提取出来的 .css 文件的名称
+  //     filename: `[name]_[contenthash:8].css`,
+  //   }),
+  // ]
 };
