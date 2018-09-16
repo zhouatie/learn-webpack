c1a957e28cc82ef5874d6e09702d3baf0664d480
zhoust <zhoust@guahao.com>
2018-09-16 14:41:21 +0800

    webpack about vue

---
+++ webpack-vue/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+        <button @click="handleClick">按钮</button>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            msg: 'Hello, world vue'
+        }
+    },
+    methods: {
+        handleClick() {
+            const message = `i use es6 to compiler | ${this.msg}`;
+            this.msg = message;
+        }
+    }
+}
+</script>
+<style>
+</style>
