11e8ef9ae71b9120727351b17ba0baefc80f73e8
zhoust <zhoust@guahao.com>
2019-04-16 00:02:51 +0800

    update study-webpack

---
+++ devServer/index.js
@@ -0,0 +1,4 @@
+import './main.less'
+require('./index.css')
+const body = document.querySelector('body')
+body.innerHTML = 5
---
+++ devServer/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: "development",
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            }
+            // {
+            //     test: /\.less$/,
+            //     use: ['style-loader', {
+            //         loader: 'css-loader',
+            //         options: {
+            //             importLoaders: 2
+            //         }
+            //     }, 'less-loader']
+            // }
+            // {
+            //     loader: 'style-loader' // creates style nodes from JS strings
+            // }, {
+            //     loader: 'css-loader' // translates CSS into CommonJS
+            // }, {
+            //     loader: 'less-loader' // compiles Less to CSS
+            // }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './index.template.html'
+        })
+    ]
+}
